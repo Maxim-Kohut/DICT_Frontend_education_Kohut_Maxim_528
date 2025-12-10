abd793fc30472378b5b38788c95b32c5f1f6f997
Maxim Kohut <papendrug@gmail.com>
2025-12-10 08:50:45 +0200

    PR12 Burger+Slider+Lang switch

--- js/main.js
+++ js/main.js
@@ -1,1 +1,47 @@
-console.log("Сайт кафедри завантажено успішно!");
+$(document).ready(function () {
+
+    $('.menu-toggle').on('click', function () {
+        $('.mobile-menu').toggleClass('show');
+    });
+
+    $('#news .news-cards').not('.slick-initialized').slick({
+        slidesToShow: 4,
+        slidesToScroll: 1,
+        infinite: true,
+        dots: true,
+        arrows: true,
+        autoplay: true,
+        autoplaySpeed: 2000,
+        responsive: [
+            { breakpoint: 1200, settings: { slidesToShow: 4 }},
+            { breakpoint: 1024,  settings: { slidesToShow: 2 }},
+            { breakpoint: 1023,  settings: { slidesToShow: 1 }}
+        ]
+    });
+
+    $('.reviews-slider').not('.slick-initialized').slick({
+        slidesToShow: 3,
+        slidesToScroll: 1,
+        arrows: true,
+        dots: false,
+        infinite: true,
+        responsive: [
+            { breakpoint: 1200, settings: { slidesToShow: 4 }},
+            { breakpoint: 1024,  settings: { slidesToShow: 2 }},
+            { breakpoint: 1023,  settings: { slidesToShow: 1 }}
+        ]
+    });
+
+    $('.lang-wrapper .ua a').on('click', function (e) {
+        e.preventDefault();
+        $('.lang-wrapper .en a').removeClass('is-active');
+        $(this).addClass('is-active');
+    });
+
+    $('.lang-wrapper .en a').on('click', function (e) {
+        e.preventDefault();
+        $('.lang-wrapper .ua a').removeClass('is-active');
+        $(this).addClass('is-active');
+    });
+
+});
